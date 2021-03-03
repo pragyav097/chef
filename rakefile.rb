5b70d0770813257d504004d4aad408f6c312e7ee
pragyav097 <68845969+pragyav097@users.noreply.github.com>
2021-03-03 14:00:00 +0530

    Update rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@
 require 'chef-api'
 include ChefAPI::Resource
 
-YOUR_CHEF_SERVER = 'https://api.chef.io/organizations/MY_ORG'
+YOUR_CHEF_SERVER = 'https://api.chef.io/organizations/pragyav'
 
 
 task :generate_jobs_file do
